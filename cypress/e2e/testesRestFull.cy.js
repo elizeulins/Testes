5b88b178d983c38dc066fc2fbe108758f6ee6ec3
elizeulins <elizeulins@hotmail.com>
2023-05-17 22:50:27 -0300

    Feat - Entrando em contato com a Acomodação

--- cypress/e2e/testesRestFull.cy.js
+++ cypress/e2e/testesRestFull.cy.js
@@ -1,12 +1,23 @@
-describe('testando', () => {
-    it('entrando no site', () => {
+describe('Entrando no site RestFull - aluguel de quartos', () => {
+    beforeEach('Acessando o site', () => {
         cy.visit('https://automationintesting.online/');
+    })
+
+    it('deve entrar em contato com o hotel por meio de passagem de info COM SUCESSO', () => {
+        cy.get('#name').type('Elizeu Lins')
+        cy.get('#email').type('Elizeu@gmail.com')
+        cy.get('#phone').type('8391122-3425')
+        cy.get('#subject').type('Aqui esta uma mensagem!')
+        cy.get('#description').type('Aqui esta a descrição da mensagem!');
+        cy.get('#submitContact').type('button').click();
+    });
+
+    it('deve entrar em contato com o hotel por meio de passagem de info SEM SUCESSO', () => {
         cy.get('#name').type('Elizeu Lins')
         cy.get('#email').type('Elizeu@gmail.com')
         cy.get('#phone').type('3425')
         cy.get('#subject').type('Aqui esta uma mensagem!')
         cy.get('#description').type('Aqui esta a descrição da mensagem!');
         cy.get('#submitContact').type('button').click();
-        //cy.get('.FPdoLc').find('[name="btnK"]').click();
-    })
+    });
 })
